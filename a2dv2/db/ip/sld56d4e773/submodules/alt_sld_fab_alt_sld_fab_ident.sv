3721cd5ea63c5f61b40499c9c1c5945e46eaae49
arya1080 <arya.chowdhury.mugdha@gmail.com>
2016-09-12 14:17:52 -0600

    Updated files for the DE0 Nano board

--- a2dv2/db/ip/sld56d4e773/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ a2dv2/db/ip/sld56d4e773/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : a849ecaa156c21102eab
+//   DESIGN_HASH   : c00a88da1344ed5d9424
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha849ecaa156c21102eab };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hc00a88da1344ed5d9424 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
